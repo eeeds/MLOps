b823501fa27cf5287ed1d5eaf940323869de3935
eeeds <encinaesteban27@gmail.com>
2022-08-31 10:32:33 -0400

    Fixing problems

--- utilities/setter.py
+++ utilities/setter.py
@@ -1,11 +1,11 @@
 import os 
-from base64 import b64encode
+from base64 import b64decode
 
 
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
-    with open('path.json', 'w') as f:
-        f.write(b64encode(key.decode()))
+    with open('path.json','w') as json_file:
+        json_file.write(b64decode(key).decode())
     print(os.path.realpath('path.json'))
 
 if __name__ == '__main__':
